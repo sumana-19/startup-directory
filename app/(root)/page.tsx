e03d0fa42577b8a522a05a74626b75ca1cdc4711
Sumana Sur <sumanasur@Sumanas-MacBook-Air.local>
2024-12-28 22:40:43 +0530

    Added Auth and Search

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+import SearchForm from "@/components/SearchForm";
+import Image from "next/image";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in the Virtual
+          Competition
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
+}
